cdf8c143c8b5d20524faf6b528e20e2ebc9bbb13
sunwoo162 <sunwoosunwoo4114@gmail.com>
2026-08-28 08:11:53 +0900

    style: load item search styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import OfficialDataApp from './OfficialDataApp';
 import './styles.css';
+import './search.css';
 
 const root = document.getElementById('root');
 
